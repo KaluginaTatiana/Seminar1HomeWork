ee4f14fa3850ecfe48f057a0bae0429df6f40471
Tatiana <ttt-ttt@bk.ru>
2022-06-27 02:46:26 +0300

    Task1HomeWorkAdditional

---
+++ Task1HomeWorkAdditional/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран
+Console.WriteLine("Введите целое число больше нуля");
+double num = Convert.ToDouble(Console.ReadLine());
+if (num % 1 == 0)
+{
+    if (num > 0)
+    {
+        while (num > 1)
+        {
+            Console.Write("1, ");
+            num = num - 1;
+        }
+        Console.WriteLine("1");
+    }
+    else
+    {
+        Console.WriteLine("Число должно быть больше нуля. Попробуйте еще раз.");
+    }
+}
+else
+{
+    Console.WriteLine("Число должно быть целым. Попробуйте еще раз.");
+}
