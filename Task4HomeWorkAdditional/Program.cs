665fb72e3a4872ae539bca181f5eac858bfae779
Tatiana <ttt-ttt@bk.ru>
2022-06-27 03:03:17 +0300

    Task4HomeWorkAdditional

---
+++ Task4HomeWorkAdditional/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано N секунд. Вывести время в формате часы:минуты:секунды.
+//N = 72334 -> 20:5:34
+int N = 72334;
+int Hour = N / 3600;
+int Min = (N % 3600) / 60;
+int Sec = (N % 3600) % 60;
+Console.Write(Hour);
+Console.Write(":");
+Console.Write(Min);
+Console.Write(":");
+Console.WriteLine(Sec);
