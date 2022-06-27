165d47236a7b837d69b6d6a08a20f02af0c57318
Tatiana <ttt-ttt@bk.ru>
2022-06-27 19:28:31 +0300

    Task2HomeWork

--- Task2HomeWork/Program.cs
+++ Task2HomeWork/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Добавила еще про равенство чисел.
+Console.WriteLine("Введите два числа");
+Console.WriteLine("Введите первое число");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double num2 = Convert.ToDouble(Console.ReadLine());
+double maxnum, minnum;
+if (num1 != num2)
+{
+    if (num1 > num2)
+    {
+        maxnum = num1;
+        minnum = num2;
+    }
+    else
+    {
+        maxnum = num2;
+        minnum = num1;
+    }
+    Console.Write("Большее число = ");
+    Console.Write(maxnum);
+    Console.Write(". ");
+    Console.Write("Меньшее число = ");
+    Console.Write(minnum);
+    Console.WriteLine(".");
+    }
+else
+{
+    Console.WriteLine("Числа равны.");
+}
