b3ae6033b4f7030defc758db0e8cdda945c30a16
Tatiana <ttt-ttt@bk.ru>
2022-06-27 03:10:23 +0300

    Task4HomeWorkAdditionalUpgrade

---
+++ Task4HomeWorkAdditionalUpgrade/Program.cs
@@ -0,0 +1,12 @@
+﻿// Перевод времени в секундах в формат часы:минуты:секунды.
+//72334 сек -> 20:5:34
+Console.WriteLine("Введите время в секундах");
+int N = Convert.ToInt32(Console.ReadLine());
+int Hour = N / 3600;
+int Min = (N % 3600) / 60;
+int Sec = (N % 3600) % 60;
+Console.Write(Hour);
+Console.Write(":");
+Console.Write(Min);
+Console.Write(":");
+Console.WriteLine(Sec);
