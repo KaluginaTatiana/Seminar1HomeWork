9e5ffc25a914d492b79d15582d601e455a5715ca
Tatiana <ttt-ttt@bk.ru>
2022-06-27 19:58:04 +0300

    Task6HomeWork

---
+++ Task6HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите любое целое число");
+double num = Convert.ToDouble(Console.ReadLine());
+if (num % 1 != 0)
+{
+    Console.WriteLine("Число должно быть целым. Попробуйте еще раз.");
+}
+else
+{
+    if (num % 2 == 0)
+    {
+        Console.Write("Число ");
+        Console.Write(num);
+        Console.WriteLine(" - четное.");
+    }
+    else
+    {
+        Console.Write("Число ");
+        Console.Write(num);
+        Console.WriteLine(" - нечетное.");
+    }
+}
