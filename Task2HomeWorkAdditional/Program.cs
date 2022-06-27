8d73646069930b23e9b708a794caa28d46af37ae
Tatiana <ttt-ttt@bk.ru>
2022-06-27 16:22:17 +0300

    Task2HomeWorkAdditional

---
+++ Task2HomeWorkAdditional/Program.cs
@@ -0,0 +1,32 @@
+﻿// Даны два числа a, b, такие что a < b. Вывести на экран сколько раз число a поместиться в числе b.
+Console.WriteLine("Введите два неравных числа");
+Console.WriteLine("Введите первое число");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double num2 = Convert.ToDouble(Console.ReadLine());
+double maxnum, minnum, div;
+if (num1 != num2)
+{
+    if (num1 > num2)
+    {
+        maxnum = num1;
+        minnum = num2;
+    }
+    else
+    {
+        maxnum = num2;
+        minnum = num1;
+    }
+    div = (maxnum - maxnum % minnum) / minnum;
+    Console.Write("Число ");
+    Console.Write(minnum);
+    Console.Write(" помещается в числе ");
+    Console.Write(maxnum);
+    Console.Write(" - ");
+    Console.Write(div);
+    Console.WriteLine(" раз(а).");
+}
+else
+{
+    Console.WriteLine("Числа должны быть неравные. Попробуйте еще раз.");
+}
